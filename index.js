b6041a24f7564719c24986f73dd65e7d718e8c53
Paweł Powroźnik <pawel.powroznik21@gmail.com>
2020-01-11 11:09:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,11 +11,14 @@ function nowServing(arr){
   }
 }
 
+
 function currentLine(line){
   if(line.length === 0){
     return "The line is currently empty.";
   }else{
+    var anwser = "The line is currently:"; 
     for(i=0; i <line.length; i++){
+      var nextPeson =
       
     } return 
   }
